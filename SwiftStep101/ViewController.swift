4fe8c1d4b8dc0f0c2c5469870eecda56d096de2b
Baki Altun <baltun@bakis-imac.local>
2017-12-13 06:03:52 -0800

    Initial Commit

---
+++ SwiftStep101/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftStep101
+//
+//  Created by Baki Altun on 13.12.2017.
+//  Copyright © 2017 Baki Altun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
